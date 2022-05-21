b563bd9627df3416ad877df2b44c0e9607b7170b
themode <themode@outlook.fr>
2022-05-21 21:38:02 +0200

    Rename statements

--- src/main/java/io/behave/Statement.java
+++ src/main/java/io/behave/Statement.java
@@ -1,22 +1,27 @@
 package io.behave;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
+import java.util.Map;
 
 sealed interface Statement
-        permits Statement.ChangeTag, Statement.Command, Statement.Request {
+        permits Statement.UpdateTag, Statement.Command, Statement.RemoteRequest {
     non-sealed interface Command extends Statement {
         @NotNull String name();
 
         @NotNull List<@NotNull Expression> arguments();
     }
 
-    non-sealed interface Request extends Statement {
-        @NotNull String input();
+    non-sealed interface RemoteRequest extends Statement {
+        @NotNull String name();
+
+        @Unmodifiable
+        @NotNull Map<String, Expression> data();
     }
 
-    non-sealed interface ChangeTag extends Statement {
+    non-sealed interface UpdateTag extends Statement {
         @NotNull BehaveObject object();
 
         @NotNull Tag tag();
